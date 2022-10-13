a56535223510b38e6b9fd1ea4f2ff35d0619d914
Mokshin Alexandr <MokshinRe@yandex.ru>
2022-10-13 21:13:33 +0300

    changed types at the Button

--- src/lib/Button/Button.tsx
+++ src/lib/Button/Button.tsx
@@ -1,17 +1,24 @@
 import { StyledButton } from './Button.styles';
 import { ButtonHTMLAttributes, MouseEventHandler } from "react";
 
-export enum Variant_Color{
+export enum Variant_Color {
   Primary = "primary",
   Success = "success",
   Warning = "warning",
   Danger = "danger",
 }
 
+export enum Size {
+  Large = 'large',
+  Medium = 'medium',
+  Small = 'small',
+}
+
 export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: Variant_Color;
   disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  size?: Size;
 };
 
 function UXButton({ children, variant, disabled, onClick }: Props) {
