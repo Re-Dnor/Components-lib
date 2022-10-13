ef236f496cc118b005670154e9ef316657285f5e
Mokshin Alexandr <MokshinRe@yandex.ru>
2022-10-13 21:16:06 +0300

    added row component

---
+++ src/lib/Row/Row.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Props } from './Row';
+
+
+export const StyledRow = styled.div<Props>`
+  display: flex;
+  align-items: ${(props) => props.alignItems};
+	justify-content: ${(props) => props.justifyContent};
+  width: ${(props) => props.fill ? '100%' : null}
+`;
---
+++ src/lib/Row/Row.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+import { StyledRow } from './Row.styles';
+
+type Justify = 'start' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | 'end';
+type Align = 'stretch' | 'center' | 'start' | 'end';
+
+
+export type Props = {
+  children?: ReactNode | ReactNode[]
+  justifyContent?: Justify;
+  alignItems?: Align;
+  fill?: boolean;
+};
+
+function UXRow({ children, justifyContent, alignItems, fill = true }: Props) {
+  console.log(justifyContent, alignItems)
+  return (
+    <StyledRow justifyContent={ justifyContent } alignItems={ alignItems } fill={fill}>
+      { children }
+    </StyledRow>
+  );
+}
+
+export default UXRow;
