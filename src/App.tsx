61546ab23af5d03838b3cb6ff345f9de4f144bd3
Mokshin Alexandr <MokshinRe@yandex.ru>
2022-10-13 21:16:26 +0300

    used components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Variant_Color } from "./lib";
 import UXButton from './lib/Button/Button';
+import UXRow from './lib/Row/Row';
+import UXColumn from "./lib/Column/Column";
 
 function App() {
   const onClickes= (variant: string) => {
@@ -7,13 +9,31 @@ function App() {
   };
 
   return (
-    <>
+    <UXColumn alignItems="start">
+      <UXRow alignItems="center" justifyContent="space-between">
+        <UXButton variant={ Variant_Color.Primary } onClick={ () => onClickes(Variant_Color.Primary) }> Primary </UXButton>
+        <UXButton variant={ Variant_Color.Success } onClick={ () => onClickes(Variant_Color.Success) }> Success </UXButton>
+        <UXButton variant={ Variant_Color.Warning } onClick={ () => onClickes(Variant_Color.Warning) }> Warning </UXButton>
+        <UXButton variant={ Variant_Color.Danger } onClick={ () => onClickes(Variant_Color.Danger) }> Danger </UXButton>
+        <UXButton  onClick={ () => onClickes(Variant_Color.Danger) } disabled> Disabled </UXButton>
+      </UXRow>
+
+      <UXRow
+        alignItems="center"
+        justifyContent="center"
+        fill
+      >
+        <UXButton variant={ Variant_Color.Primary } onClick={ () => onClickes(Variant_Color.Primary) }> Primary </UXButton>
+      </UXRow>
+
+      <UXRow
+      alignItems="end"
+      justifyContent="end"
+      fill={false}
+      >
       <UXButton variant={ Variant_Color.Primary } onClick={ () => onClickes(Variant_Color.Primary) }> Primary </UXButton>
-      <UXButton variant={ Variant_Color.Success } onClick={ () => onClickes(Variant_Color.Success) }> Success </UXButton>
-      <UXButton variant={ Variant_Color.Warning } onClick={ () => onClickes(Variant_Color.Warning) }> Warning </UXButton>
-      <UXButton variant={ Variant_Color.Danger } onClick={ () => onClickes(Variant_Color.Danger) }> Danger </UXButton>
-      <UXButton  onClick={ () => onClickes(Variant_Color.Danger) } disabled> Disabled </UXButton>
-    </>
+      </UXRow>
+    </UXColumn>
   )
 }
 
