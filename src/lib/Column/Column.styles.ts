d90fd9e160344f82383b59636acd40f2faf0b266
Mokshin Alexandr <MokshinRe@yandex.ru>
2022-10-13 21:15:54 +0300

    added column component

---
+++ src/lib/Column/Column.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Props } from './Column';
+
+
+export const StyledColumn = styled.div<Props>`
+  display: flex;
+  flex-direction: column;
+  align-items: ${(props) => props.alignItems};
+	justify-content: ${(props) => props.justifyContent};
+`;
---
+++ src/lib/Column/Column.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+import { StyledColumn } from './Column.styles';
+
+type Justify = 'start' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | 'end';
+type Align = 'stretch' | 'center' | 'start' | 'end';
+
+
+export type Props = {
+  children?: ReactNode | ReactNode[];
+  justifyContent?: Justify;
+  alignItems?: Align;
+};
+
+function UXColumn({ children, alignItems, justifyContent }: Props) {
+  return ( 
+    <StyledColumn alignItems={alignItems} justifyContent={justifyContent}>
+      { children }
+    </StyledColumn>
+  );
+}
+
+export default UXColumn;
