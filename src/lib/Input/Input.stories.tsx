318d5faefd0aea5ffabe8efc4d23394413789722
Mokshin Alexandr <MokshinRe@yandex.ru>
2022-10-24 21:12:31 +0300

    added input stories

---
+++ src/lib/Input/Input.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import Input from "./Input";
+import "../../index.css";
+
+
+const meta: ComponentMeta<typeof Input> = {
+  title: "Design System/Input",
+  component: Input
+};
+export default meta;
+
+
+
+export const Default: ComponentStoryObj<typeof Input> = {
+  args: {
+    type: "name",
+    name: "name",
+    value: "Mike",
+    large: false
+  }
+};
+
+export const Large: ComponentStoryObj<typeof Input> = {
+  args: {
+    type: "email",
+    value: "Mike@mail.com",
+    large: true
+  }
+};
