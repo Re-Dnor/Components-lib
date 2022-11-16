61b942cda8b056bfe838981c304c5fc532c3c049
Mokshin Alexandr <MokshinRe@yandex.ru>
2022-11-16 14:55:42 +0300

    add corrections

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,8 @@
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     react(),
-    dts({
-      insertTypesEntry: true
-    }),
   ],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "styled-components"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "styled-components": "styled",
-        },
-      },
-    },
-  },
 });
